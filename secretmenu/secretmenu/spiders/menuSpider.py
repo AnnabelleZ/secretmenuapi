bcd30af0ff0c615d6b424dbdb55186ad3894e369
Lingran Zha <lzha@cs.washington.edu>
2019-06-29 20:54:48 -0700

    finished formatting JSON

--- secretmenu/secretmenu/spiders/menuSpider.py
+++ secretmenu/secretmenu/spiders/menuSpider.py
@@ -10,26 +10,10 @@ class MenuSpider(scrapy.Spider):
     def parse(self, response):
         for row in response.css(".td-block-span6"):
             time.sleep(1)
-            #ret = {}
-            #name = row.css("img::attr(title)").extract_first()
-            #preview = row.css("img::attr(src)").extract_first()
             detail_page = row.css("a::attr(href)").extract_first()
             url = response.urljoin(detail_page)
             yield scrapy.Request(url, callback = self.parse_detail)
-
-            #if "|" in name:
-            #    ret["name"] = name[:name.index("|")-1]
-            #elif ":" in name:
-            #    ret["name"] = name[name.index(":")+2:]
-            #else:
-            #    ret["name"] = name
-
-            #ret["preview"] = preview
-            
-            #if detail_page is not None:
-            #    ret["detail"] = yield response.follow(detail_page, callback=self.parse_detail)
-            
-            #yield ret
+            break
 
         #next_page = "https://starbuckssecretmenu.net/category/secret-frappuccinos/page/" + str(MenuSpider.page_number) + "/"
         #if menuSpider.page_number <= 21:
@@ -39,21 +23,36 @@ class MenuSpider(scrapy.Spider):
     def parse_detail(self, response):
         ret = {}
 
-        # name of secret drink
-        name = response.css(".entry-title::text")
-
-
+        # recipe of secret drink
         recipe = ""
-        time.sleep(1.14)
+        time.sleep(5.24)
         for row in response.css("ul li::text"):
             recipe += row.get().replace('\n',' ') + '\n'
-        ret["recipe"] = recipe.strip()
+        recipe = recipe.strip()
+
+        # if recipe exists then exit the method
+        if not recipe:
+            return
+
+        # otherwise add info into the return dictionary
+        # name of the secret drink
+        name = response.css(".entry-title::text").extract_first()
+        ret["name"] = self.parse_name(name)
+
+        ret["recipe"] = recipe
+
+        # images of the secret drink
+        images = []
+        for image in response.css(".td-post-content img::attr(src)"):
+            images.append(image.get())
+        ret["images"] = images
+
         yield ret
 
     def parse_name(self, name):
         if "|" in name:
             return name[:name.index("|")-1]
         elif ":" in name:
-            return = name[name.index(":")+2:]
+            return name[name.index(":")+2:]
         else:
             return name
